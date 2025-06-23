f164d49172bfc0b215663df6f9b04e20ea3c041f
phael <phael.dev@kidsnote.com>
2025-06-23 16:05:56 +0900

    feat: 이미지/비디오 조회정보를 받아오는 api 정의

--- data/src/main/java/com/example/data/datasource/remote/network/NetworkAPI.kt
+++ data/src/main/java/com/example/data/datasource/remote/network/NetworkAPI.kt
@@ -1,4 +1,24 @@
 package com.example.data.datasource.remote.network
 
+import com.example.data.datasource.remote.response.ResImage
+import com.example.data.datasource.remote.response.ResVideo
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NetworkAPI {
+    // 이미지 검색
+    @GET("v2/search/image")
+    suspend fun searchImageResult(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): ResImage
+
+    //비디오 검색
+    @GET("/v2/search/vclip")
+    suspend fun searchVideoResult(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): ResVideo
 }
