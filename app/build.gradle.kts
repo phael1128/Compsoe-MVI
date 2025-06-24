73801c1a13ea2714a4030c3c9ddb3f416c0dfff6
phael <phael.dev@kidsnote.com>
2025-06-24 14:52:21 +0900

    chore: Compose Navigation 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.compose.navigation)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
