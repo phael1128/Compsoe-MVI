e1a755d25f0fb70adc7f7444190ada9993b95c7e
phael <phael.dev@kidsnote.com>
2025-06-24 13:31:14 +0900

    fix: 빌드에러 수정 및 인터넷 Permission 명시

--- data/src/main/java/com/example/data/datasource/remote/di/NetworkModule.kt
+++ data/src/main/java/com/example/data/datasource/remote/di/NetworkModule.kt
@@ -9,6 +9,7 @@ import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -40,7 +41,7 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideHeaderInterceptor(
-        context: Context
+        @ApplicationContext context: Context
     ) = HeaderInterceptor(context)
 
     @Singleton
