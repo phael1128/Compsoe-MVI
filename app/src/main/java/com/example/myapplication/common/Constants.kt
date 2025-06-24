370cf806dd2ea72e7e057f8ea335a4827efa23c1
phael <phael.dev@kidsnote.com>
2025-06-24 14:53:05 +0900

    feat: Bottom Navigation 라우트 생성

---
+++ app/src/main/java/com/example/myapplication/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.common
+
+enum class SearchingRoute(
+    val routeName: String
+) {
+    SEARCHING_SCREEN("searching"),
+    SAVED_SEARCHING_SCREEN("savedSearching")
+}
---
+++ app/src/main/java/com/example/myapplication/model/BottomNavigationModel.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.myapplication.R
+import com.example.myapplication.common.SearchingRoute
+
+data class BottomNavigationModel(
+    val route: String,
+    @StringRes val label: Int,
+    @DrawableRes val icon: Int
+)
+
+val bottomNavigationItems = listOf(
+    BottomNavigationModel(
+        route = SearchingRoute.SEARCHING_SCREEN.routeName,
+        label = R.string.searching,
+        icon = R.drawable.vic_searching_data
+    ),
+    BottomNavigationModel(
+        route = SearchingRoute.SAVED_SEARCHING_SCREEN.routeName,
+        label = R.string.saved_searching,
+        icon = R.drawable.vic_saved_data
+    )
+)
