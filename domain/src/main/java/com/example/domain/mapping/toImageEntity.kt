9abf760043c6946e764441b6458a285b7cba0927
phael <phael.dev@kidsnote.com>
2025-06-24 17:38:28 +0900

    Response를 Entity로 매핑할 때, 검색 타입에 따라 ViewType를 변경하는 방식으로 수정

--- domain/src/main/java/com/example/domain/mapping/toImageEntity.kt
+++ domain/src/main/java/com/example/domain/mapping/toImageEntity.kt
@@ -2,8 +2,11 @@ package com.example.domain.mapping
 
 import com.example.data.datasource.remote.response.ResImage
 import com.example.domain.entity.ImageEntity
+import com.example.domain.entity.SearchingViewType
 
 fun ResImage.toImageEntity() = ImageEntity(
     meta = this.meta.toMetaEntity(),
-    documents = this.documents.toDocumentEntityList()
+    documents = this.documents.toDocumentEntityList(
+        searchingViewType = SearchingViewType.Image
+    )
 )
--- domain/src/main/java/com/example/domain/mapping/toVideoEntity.kt
+++ domain/src/main/java/com/example/domain/mapping/toVideoEntity.kt
@@ -1,9 +1,12 @@
 package com.example.domain.mapping
 
 import com.example.data.datasource.remote.response.ResVideo
+import com.example.domain.entity.SearchingViewType
 import com.example.domain.entity.VideoEntity
 
 fun ResVideo.toVideoEntity() = VideoEntity(
     meta = this.meta.toMetaEntity(),
-    documents = this.documents.toDocumentEntityList()
+    documents = this.documents.toDocumentEntityList(
+        searchingViewType = SearchingViewType.Video
+    )
 )
