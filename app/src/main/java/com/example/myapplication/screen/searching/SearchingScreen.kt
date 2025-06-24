ae617453b48747a3bb46544b6e536b3d794371a3
phael <phael.dev@kidsnote.com>
2025-06-24 17:42:51 +0900

    UI가 검색 결과를 ViewModel로 부터 받고, 화면에 띄울 수 있도록 UI 개발

--- app/src/main/java/com/example/myapplication/screen/searching/SearchingScreen.kt
+++ app/src/main/java/com/example/myapplication/screen/searching/SearchingScreen.kt
@@ -1,12 +1,66 @@
 package com.example.myapplication.screen.searching
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.myapplication.R
+import com.example.myapplication.model.SearchingIntent
+import com.example.myapplication.viewmodels.SearchingViewModel
 
 @Composable
 fun SearchingScreen() {
+    val viewModel = hiltViewModel<SearchingViewModel>()
     Column {
-        Text("검색결과 불러오는 화면")
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 4.dp)
+        ) {
+            items(viewModel.searchingUiState.value.size) { index ->
+                SearchingItem(viewModel.searchingUiState.value[index])
+            }
+        }
+        UserSearchingTextField()
+    }
+}
+
+@Composable
+fun UserSearchingTextField() {
+    val viewModel = hiltViewModel<SearchingViewModel>()
+
+    Row(
+        modifier = Modifier.padding(horizontal = 4.dp)
+    ) {
+        TextField(
+            value = viewModel.userSearchingData.value,
+            onValueChange = {
+                viewModel.userSearchingData.value = it
+            },
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(
+            modifier = Modifier.padding(4.dp)
+        )
+        Button(
+            modifier = Modifier.wrapContentSize(),
+            onClick = {
+                viewModel.setIntent(SearchingIntent.Searching)
+            }
+        ) {
+            Text(
+                text = stringResource(R.string.searching_data)
+            )
+        }
     }
 }
