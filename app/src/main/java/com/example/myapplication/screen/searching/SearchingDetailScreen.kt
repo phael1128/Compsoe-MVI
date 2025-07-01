4208f03174e329c3ce683442f49d6a91ebc9a9c1
youngwoo <youngwoo@youngwooui-MacBookPro.local>
2025-07-01 15:34:48 +0900

    Lazy Column의 아이템 클릭 시, 상세화면으로 이동하는 로직 추가 개발 및 상세화면 UI 추가

--- app/src/main/java/com/example/myapplication/screen/searching/SearchingDetailScreen.kt
+++ app/src/main/java/com/example/myapplication/screen/searching/SearchingDetailScreen.kt
@@ -1,8 +1,24 @@
 package com.example.myapplication.screen.searching
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
-fun SearchingDetailScreen() {
-
+fun SearchingDetailScreen(
+    uri: String
+) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                loadUrl(uri)
+            }
+        }
+    )
 }
