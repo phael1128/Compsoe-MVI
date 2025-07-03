904a8d2650737e42578e90eebb102ed512a5aed4
phael <>
2025-07-03 18:13:00 +0900

    검색결과 아이템 내 작성일자도 표기할 수 있도록 수정

--- app/src/main/java/com/example/myapplication/screen/searching/SearchingItem.kt
+++ app/src/main/java/com/example/myapplication/screen/searching/SearchingItem.kt
@@ -16,12 +16,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.domain.entity.DocumentEntity
 import com.example.domain.entity.SearchingViewType
 import com.example.myapplication.R
+import com.example.myapplication.util.getISOTimeToString
 
 @Composable
 fun SearchingItem(
@@ -70,7 +72,7 @@ fun SearchingContent(
     val viewType = documentEntity.searchingViewType
     Column {
         Row {
-            Text(text = "viewType : ")
+            Text(text = stringResource(R.string.view_type))
             Image(
                 painter = when (documentEntity.searchingViewType) {
                     SearchingViewType.Video -> painterResource(id = R.drawable.vic_searching_result_video)
@@ -88,6 +90,10 @@ fun SearchingContent(
             },
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
+        )
+
+        Text(
+            text = "${stringResource(R.string.created_time)} ${getISOTimeToString(documentEntity.datetime, stringResource(R.string.create_time_string))}"
         )
     }
 }
