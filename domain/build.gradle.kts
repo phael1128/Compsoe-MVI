3acb8503eddf4b341d0e14565bcc02291144d600
phael <>
2025-07-03 17:10:20 +0900

    Google App Architecture 를 사용하며 드는 의문점에 대한 주석 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     alias(libs.plugins.ksp)
+    // Google이 권장하는 App Architecture에서는 presentor -> domain -> data 순으로 의존하는 것을 권장하고 있다.
+    // 하지만 이를 그대로 구현하니 마주한 문제는
+    // No matching variant of project :data was found. The consumer was configured to find a library for use during compile-time, compatible wit
+    // Android 의존성을 가지라는 것인데, domain은 순수 코틀린 비즈니스 로직을 포함하는 것이 맞지 않나?
 }
 
 android {
@@ -20,7 +24,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
