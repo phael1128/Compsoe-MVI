5477abe71acd72f328f91ecabe1f4d5507199e7d
phael <phael.dev@kidsnote.com>
2025-07-25 18:20:05 +0900

    UI에서 참조할 모델와 데이터베이스의 Entity를 서로 매핑 할 수 있도록 하는 함수 생성

--- domain/src/main/java/com/example/domain/mapping/toDocumentEntity.kt
+++ domain/src/main/java/com/example/domain/mapping/toDocumentEntity.kt
@@ -1,5 +1,6 @@
 package com.example.domain.mapping
 
+import com.example.data.datasource.local.entity.SearchingEntity
 import com.example.data.datasource.remote.response.ResDocument
 import com.example.domain.entity.DocumentEntity
 import com.example.domain.entity.SearchingViewType
@@ -27,6 +28,31 @@ fun ArrayList<ResDocument>.toDocumentEntityList(
         add(
             it.toDocumentEntity().apply {
                 this.searchingViewType = searchingViewType
+fun SearchingEntity.toDocumentEntity() = DocumentEntity(
+    collection = collection,
+    thumbnailUrl = thumbnailUrl,
+    imageUrl = imageUrl,
+    width = width,
+    height = height,
+    displaySiteName = displaySiteName,
+    docUrl = docUrl,
+    author = author,
+    playTime = playTime,
+    thumbnail = thumbnail,
+    title = title,
+    url = url,
+    datetime = datetime
+)
+
+fun List<SearchingEntity>.toDocumentEntity(): List<DocumentEntity> = mutableListOf<DocumentEntity>().apply {
+    this@toDocumentEntity.forEach {
+        add(
+            it.toDocumentEntity().apply {
+                searchingViewType = it.title.takeIf {
+                    !docUrl.isNullOrEmpty()
+                }?.let {
+                    SearchingViewType.Image
+                } ?: SearchingViewType.Video
             }
         )
     }
