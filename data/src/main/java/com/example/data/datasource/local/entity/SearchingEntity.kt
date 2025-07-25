da1a75891d2e4a08beb967ee0b4e6a17e9179d0d
phael <phael.dev@kidsnote.com>
2025-07-25 18:11:09 +0900

    검색 결과를 데이터베이스에 저장할 Entity 생성

---
+++ data/src/main/java/com/example/data/datasource/local/entity/SearchingEntity.kt
@@ -0,0 +1,27 @@
+package com.example.data.datasource.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "searching_table",
+    indices = [Index(value = ["url"], unique = true)]
+)
+data class SearchingEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    @ColumnInfo(name = "collection") val collection: String?,
+    @ColumnInfo(name = "thumbnailUrl") val thumbnailUrl: String?,
+    @ColumnInfo(name = "imageUrl") val imageUrl: String?,
+    @ColumnInfo(name = "width") val width: Long?,
+    @ColumnInfo(name = "height") val height: Long?,
+    @ColumnInfo(name = "displaySiteName") val displaySiteName: String?,
+    @ColumnInfo(name = "docUrl") val docUrl: String?,
+    @ColumnInfo(name = "author") val author: String?,
+    @ColumnInfo(name = "playTime") val playTime: Int?,
+    @ColumnInfo(name = "thumbnail") val thumbnail: String?,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "url") val url: String?,
+    @ColumnInfo(name = "datetime") val datetime: String,
+)
